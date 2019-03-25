c09e4b0ccd12cd65cbfd773c867ebf02eb971236
Jake Webb <jakewebbsite@gmail.com>
2019-03-25 16:33:06 -0500

    add nav bar with items and redirect on click

---
+++ src/components/NavBar/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const NavContainer = styled.div`
+  position: fixed;
+  background: #eeeeee;
+  height: 50px;
+  text-align: center;
+  width: 100%;
+`
+export const NavItem = styled.div`
+  display: inline-block;
+  font-size: 20px;
+  margin: 0px 20px;
+  color: black;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  cursor: pointer;
+`
