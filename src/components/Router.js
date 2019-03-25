111ed3e0dbbb807514e424e21576f2b106531186
Jake Webb <jakewebbsite@gmail.com>
2019-03-25 16:42:57 -0500

    add downloads route

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,25 +3,24 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
 import Connections from '../pages/Connections'
 import DataMappings from '../pages/DataMappings'
+import Downloads from '../pages/Downloads'
 import History from '../pages/History'
 import Login from '../pages/Login'
 import Transfers from '../pages/Transfers'
 
-class Router extends React.Component {
-  render () {
-    // TODO add authentication to routes?
-    return (
-      <Switch>
-        <Route path='/connections' component={Connections} />
-        <Route path='/data-mappings' component={DataMappings} />
-        <Route path='/history' component={History} />
-        <Route path='/login' component={Login} />
-        <Route path='/transfers' component={Transfers} />
-        <Route path='/' render={() => <Redirect to='/history' />} />
-        <Route path='*' render={() => <Redirect to='/login' />} />
-      </Switch>
-    )
-  }
+function Router() {
+  return (
+    <Switch>
+      <Route path='/connections' component={Connections} />
+      <Route path='/data-mappings' component={DataMappings} />
+      <Route path='/history' component={History} />
+      <Route path='/login' component={Login} />
+      <Route path='/transfers' component={Transfers} />
+      <Route path='/downloads' component={Downloads} />
+      <Route path='/' render={() => <Redirect to='/history' />} />
+      <Route path='*' render={() => <Redirect to='/login' />} />
+    </Switch>
+  )
 }
 
 export default Router
---
+++ src/pages/Downloads.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Downloads extends React.Component {
+  render () {
+    return (
+      <div>this page shows downloads</div>
+    )
+  }
+}
+
+export default Downloads
