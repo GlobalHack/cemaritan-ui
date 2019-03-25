b7e7da54553dc4f0e047a8e562122d87ab6f2719
Jake Webb <jakewebbsite@gmail.com>
2019-03-25 15:55:20 -0500

    move pages dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import NavBar from './components/NavBar'
 import Router from './components/Router'
 
 class App extends Component {
   render() {
     return (
-      <Router />
-    );
+      <div>
+        <NavBar />
+        <Router />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
