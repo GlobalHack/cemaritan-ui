b196832a8e5c7b6a531dcdb0e2daeb790ed55f7f
Kelly Hopson <khopson@1904labs.com>
2019-03-25 16:59:20 -0500

    Adding a folder for the transfer views. Adding the blank pages for create and upload transfers to the project. Adding to the router but needs to be updated to be subroutes?

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
-import Connections from '../pages/Connections'
-import DataMappings from '../pages/DataMappings'
-import Downloads from '../pages/Downloads'
-import History from '../pages/History'
-import Login from '../pages/Login'
-import Transfers from '../pages/Transfers'
+import Connections from "../pages/Connections";
+import DataMappings from "../pages/DataMappings";
+import Downloads from "../pages/Downloads";
+import History from "../pages/History";
+import Login from "../pages/Login";
+import Transfers from "../pages/transfers/Transfers";
+import UploadTransfer from "../pages/transfers/UploadTransfer";
+import CreateTransfer from "../pages/transfers/CreateTransfer";
 
 function Router() {
   return (
     <Switch>
-      <Route path='/connections' component={Connections} />
-      <Route path='/data-mappings' component={DataMappings} />
-      <Route path='/history' component={History} />
-      <Route path='/login' component={Login} />
-      <Route path='/transfers' component={Transfers} />
-      <Route path='/downloads' component={Downloads} />
-      <Route path='/' render={() => <Redirect to='/history' />} />
-      <Route path='*' render={() => <Redirect to='/login' />} />
+      <Route path="/connections" component={Connections} />
+      <Route path="/data-mappings" component={DataMappings} />
+      <Route path="/history" component={History} />
+      <Route path="/login" component={Login} />
+      <Route path="/transfers" component={Transfers} />
+      <Route path="/create-transfer" component={CreateTransfer} />
+      <Route path="/upload-transfer" component={UploadTransfer} />
+      <Route path="/downloads" component={Downloads} />
+      <Route path="/" render={() => <Redirect to="/history" />} />
+      <Route path="*" render={() => <Redirect to="/login" />} />
     </Switch>
-  )
+  );
 }
 
-export default Router
+export default Router;
