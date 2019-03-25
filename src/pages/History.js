67d4ec1b35d72f2f69b5dd3c1e42e9a8ed8bdb8d
Katie Mathews <ktmathews89@gmail.com>
2019-03-25 17:03:16 -0500

    ... start to history table

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const products = [
+  {
+    id: 1,
+    date: '2019-03-25',
+    name: 'Case-Worthy-To-HUD',
+    type: 'Transfer',
+    source: {name: 'Case Worthy'},
+    destination: {name: 'HUD'}
+  }
+];
+
+const columns = [{
+  dataField: 'date',
+  text: 'Date'
+}, {
+  dataField: 'name',
+  text: 'Name'
+}, {
+  dataField: 'type',
+  text: 'Type'
+}, {
+  dataField: 'source.name',
+  text: 'Source'
+}, {
+  dataField: 'destination.name',
+  text: 'Destination'
+}];
 
 class History extends React.Component {
+
   render () {
     return (
-      <div>this page shows History</div>
+      <div>
+        <h1>History</h1>
+        <BootstrapTable keyField='date' data={ products } columns={ columns } />
+      </div>
     )
   }
 }
