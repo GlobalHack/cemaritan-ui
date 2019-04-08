4ca0409be32ec49298628bc9b9a559308a19984f
Katie Mathews <ktmathews89@gmail.com>
2019-04-08 15:54:37 -0500

    sorting with history & downloads table

--- src/pages/Downloads.js
+++ src/pages/Downloads.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+
+const downloads = [
+  {
+    id: 1,
+    date: '2019-03-25',
+    name: 'CW-Weekly-190325',
+    source: {name: 'Case Worthy'},
+    mapping: {name: 'CW-HUD'}
+  }
+];
+
+const startDownload = (e) => {
+  e.preventDefault();
+  const downloadId = e.currentTarget.getAttribute('data-download-id');
+  console.log(`start download with id: ${downloadId}`);
+}
+
+const downloadsColumn = (cell) => (
+  <a href="" onClick={startDownload} data-download-id={cell}>Download</a>
+);
+
+const columns = [{
+  dataField: 'date',
+  text: 'Date',
+  sort: true
+}, {
+  dataField: 'name',
+  text: 'Name',
+  sort: true
+},{
+  dataField: 'source.name',
+  text: 'Source',
+  sort: true
+}, {
+  dataField: 'mapping.name',
+  text: 'Mapping',
+  sort: true
+}, {
+  dataField: 'id',
+  text: 'Download',
+  formatter: downloadsColumn
+}];
 
 class Downloads extends React.Component {
   render () {
     return (
-      <div>this page shows downloads</div>
+      <div>
+        <h1>Downloads</h1>
+        <BootstrapTable
+          bootstrap4
+          keyField='date'
+          data={ downloads }
+          columns={ columns }
+        />
+      </div>
     )
   }
 }
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 
-const products = [
+const history = [
   {
     id: 1,
     date: '2019-03-25',
+    name: 'Case-Worthy-To-HUD',
+    type: 'Transfer',
+    source: {name: 'Case Worthy'},
+    destination: {name: 'HUD'}
+  },
+  {
+    id: 2,
+    date: '2019-03-29',
     name: 'Case-Worthy-To-HUD',
     type: 'Transfer',
     source: {name: 'Case Worthy'},
@@ -14,19 +22,24 @@ const products = [
 
 const columns = [{
   dataField: 'date',
-  text: 'Date'
+  text: 'Date',
+  sort: true
 }, {
   dataField: 'name',
-  text: 'Name'
+  text: 'Name',
+  sort: true
 }, {
   dataField: 'type',
-  text: 'Type'
+  text: 'Type',
+  sort: true
 }, {
   dataField: 'source.name',
-  text: 'Source'
+  text: 'Source',
+  sort: true
 }, {
   dataField: 'destination.name',
-  text: 'Destination'
+  text: 'Destination',
+  sort: true
 }];
 
 class History extends React.Component {
@@ -35,7 +48,12 @@ class History extends React.Component {
     return (
       <div>
         <h1>History</h1>
-        <BootstrapTable keyField='date' data={ products } columns={ columns } />
+        <BootstrapTable
+          bootstrap4
+          keyField='date'
+          data={ history }
+          columns={ columns }
+        />
       </div>
     )
   }
