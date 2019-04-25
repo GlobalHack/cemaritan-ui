09b3b5007abf8ef438a0b8fa595cd2aac1ce2b47
Katie Mathews <ktmathews89@gmail.com>
2019-04-25 15:44:01 -0500

    fix to get transfers data displaying in table

--- src/pages/transfers/ViewTransfers.js
+++ src/pages/transfers/ViewTransfers.js
@@ -3,72 +3,28 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import { fetcher } from '../../utils/fetcher'
 
 
-const transfers = [
-  {
-    "UID": 1,
-    "Name": "CW to SF",
-    "CreatedDate": "2019-03-20 20:42:03",
-    "CreatedBy": 1,
-    "Organization": 1,
-    "Source": {
-      "UID": 2,
-      "Name": "SalesForce"
-    },
-    "SourceMapping": 2,
-    "Destination": {
-      "UID": 1,
-      "Name": 'CW'
-    },
-    "DestinationMapping": 1,
-    "StartDateTime": "2019-03-13 20:42:03",
-    "Frequency": "1 day",
-    "RecordFilter": "filter a",
-    "Active": 1
-  }, {
-    "UID": 2,
-    "Name": "SF to CW",
-    "CreatedDate": "2019-03-13 20:42:03",
-    "CreatedBy": 1,
-    "Organization": 1,
-    "Source": {
-      "UID": 1,
-      "Name": 'CW'
-    },
-    "SourceMapping": 1,
-    "Destination": {
-      "UID": 2,
-      "Name": 'SalesForce'
-    },
-    "DestinationMapping": 2,
-    "StartDateTime": "2019-03-13 20:42:03",
-    "Frequency": "1 hour",
-    "RecordFilter": "filter b",
-    "Active": 0
-  }
-];
-
 const columns = [{
-  dataField: 'UID',
+  dataField: 'uid',
   text: 'UID',
   hidden: true
 }, {
-  dataField: 'Active',
+  dataField: 'active',
   text: 'Active',
   sort: true,
   formatter: (cell) => cell ? 'Active' : 'Inactive'
 }, {
-  dataField: 'Source',
+  dataField: 'source',
   text: 'Source',
   sort: true
 },{
-  dataField: 'Destination',
+  dataField: 'destination',
   text: 'Destination',
   sort: true
 }, {
-  dataField: 'Frequency',
+  dataField: 'frequency',
   text: 'Frequency'
 }, {
-  dataField: 'StartDateTime',
+  dataField: 'startdatetime',
   text: 'Start Date',
   sort: true
 }, {
@@ -85,7 +41,7 @@ class ViewTransfers extends React.Component {
       data: []
     }
   }
-  
+
   componentDidMount () {
     fetcher('transfers').then(data => {
       this.setState({ data })
@@ -98,7 +54,7 @@ class ViewTransfers extends React.Component {
         <h1>Scheduled Transfers</h1>
         <BootstrapTable
           bootstrap4
-          keyField='UID'
+          keyField='uid'
           data={ this.state.data }
           columns={ columns }
         />
