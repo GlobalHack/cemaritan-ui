373bc22e980f3b541ba818dfe10e9c3d428143cd
Katie Mathews <ktmathews89@gmail.com>
2019-04-25 15:53:01 -0500

    fetch and display view connections data

--- src/pages/connections/ViewConnections.js
+++ src/pages/connections/ViewConnections.js
@@ -1,65 +1,53 @@
 import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
+import { fetcher } from '../../utils/fetcher';
 
-const connections = [
-  {
-    "UID": 1,
-    "Organization": 1,
-    "Name": "SF",
-    "CreatedDate": "2019-03-09 20:42:03",
-    "CreatedBy": 1,
-    "Type": "A",
-    "ConnectionInfo": "{conn string}"
-  }, {
-    "UID": 2,
-    "Organization": 1,
-    "Name": "CW",
-    "CreatedDate": "2019-03-10 04:42:03",
-    "CreatedBy": 1,
-    "Type": "B",
-    "ConnectionInfo": "{conn string}"
-  }, {
-    "UID": 6,
-    "Organization": 1,
-    "Name": "Secure Download",
-    "CreatedDate": "2019-03-23 20:42:03",
-    "CreatedBy": 0,
-    "Type": "F",
-    "ConnectionInfo": 0
-  }
-];
 
 const columns = [{
-  dataField: 'UID',
+  dataField: 'uid',
   text: 'UID',
   hidden: true
 }, {
-  dataField: 'Name',
+  dataField: 'name',
   text: 'Name',
   sort: true
 }, {
-  dataField: 'Type',
+  dataField: 'type',
   text: 'Type',
   sort: true
 },{
-  dataField: 'CreatedDate',
+  dataField: 'createddate',
   text: 'Created',
   sort: true
 }, {
-  dataField: 'UID',
+  dataField: 'uid',
   text: 'Edit',
   formatter: (cell) => <a href="#route-to-connection">Edit</a>
 }];
 
 class ViewConnections extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount () {
+    fetcher('connections').then(data => {
+      this.setState({ data })
+    })
+  }
+
   render () {
     return (
       <div>
         <h1>View Connections</h1>
         <BootstrapTable
           bootstrap4
-          keyField='UID'
-          data={ connections }
+          keyField='uid'
+          data={ this.state.data }
           columns={ columns }
         />
       </div>
