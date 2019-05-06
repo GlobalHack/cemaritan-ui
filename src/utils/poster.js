6c666424d37e8573483150e2899ce8cf2d22ae14
Jake Webb <jakewebbsite@gmail.com>
2019-05-06 16:53:08 -0500

    initialize poster

---
+++ src/utils/poster.js
@@ -0,0 +1,33 @@
+import config from '../config'
+
+// env is harcoded until we have two environments
+const ENV = 'nonprod'
+// this api base url is pulled from the config file imported above
+const api = config[ENV].api
+
+// organizations (options: 1, 2, 3)
+const organization_id = 1;
+
+// strings used as poster arg to map to each endpoint ex: poster('connections')
+const endpoints = {
+  transfers: `/organizations/${organization_id}/transfers`
+}
+
+const poster = (endpoint, data, passedOpts) => {
+  const opts = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(data),
+    ...passedOpts
+  }
+  // check to make sure passed endpoint is valid in endpoints object above
+  const url = !!endpoints[endpoint] ? `${api}${endpoints[endpoint]}` : console.error(`'${endpoint}' is not a valid endpoint!`)
+
+  return fetch(url, opts)
+    .then(res => res.json())
+    .catch(console.error)
+}
+
+export default poster
