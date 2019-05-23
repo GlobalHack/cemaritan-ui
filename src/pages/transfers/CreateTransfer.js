48771405587e82e98c514ab58c1d2d30e644aeeb
Katie Mathews <ktmathews89@gmail.com>
2019-05-23 15:31:51 -0500

    start of styling form

--- src/pages/transfers/CreateTransfer.js
+++ src/pages/transfers/CreateTransfer.js
@@ -1,5 +1,16 @@
 import React, {useState, useEffect} from "react";
 import {Formik, Field} from "formik";
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: block;
+  color: purple;
+`;
+
+const Select = styled.select`
+  width: 100%;
+`;
+
 
 function CreateTransfer() {
   const [sourceOptions, setSourceOptions] = useState([]);
@@ -56,48 +67,60 @@ function CreateTransfer() {
         }) => (
           <form onSubmit={handleSubmit}>
 
-            <label htmlFor="source">Source</label>
-            <Field component="select" name="source" onChange={handleChange} value={values.source}>
-              <option value=""></option>
-              {sourceOptions.map((source) => (
-                <option key={`source-option-${source.id}`} value={source.id}>{source.name}</option>
-              ))}
-            </Field>
+            <div className="form-group">
+              <Label htmlFor="source">Source</Label>
+              <Field component="Select" name="source" onChange={handleChange} value={values.source}>
+                <option value=""></option>
+                {sourceOptions.map((source) => (
+                  <option key={`source-option-${source.id}`} value={source.id}>{source.name}</option>
+                ))}
+              </Field>
+            </div>
 
-            <label htmlFor="sourceMapping">Source Mapping</label>
-            <Field component="select" name="sourceMapping" onChange={handleChange} value={values.sourceMapping}>
-              <option value=""></option>
-              {sourceMappingOptions.map((mapping) => (
-                <option key={`source-mapping-option-${mapping.id}`} value={mapping.id}>{mapping.name}</option>
-              ))}
-            </Field>
+            <div className="form-group">
+              <Label htmlFor="sourceMapping">Source Mapping</Label>
+              <Field component="select" name="sourceMapping" onChange={handleChange} value={values.sourceMapping}>
+                <option value=""></option>
+                {sourceMappingOptions.map((mapping) => (
+                  <option key={`source-mapping-option-${mapping.id}`} value={mapping.id}>{mapping.name}</option>
+                ))}
+              </Field>
+            </div>
 
-            <label htmlFor="destination">Destination</label>
-            <Field component="select" name="destination" onChange={handleChange} value={values.destination}>
-              <option value=""></option>
-              {destinationOptions.map((dest) => (
-                <option key={`dest-option-${dest.id}`} value={dest.id}>{dest.name}</option>
-              ))}
-            </Field>
+            <div className="form-group">
+              <Label htmlFor="destination">Destination</Label>
+              <Field component="select" name="destination" onChange={handleChange} value={values.destination}>
+                <option value=""></option>
+                {destinationOptions.map((dest) => (
+                  <option key={`dest-option-${dest.id}`} value={dest.id}>{dest.name}</option>
+                ))}
+              </Field>
+            </div>
 
-            <label htmlFor="destinationMapping">Destination Mapping</label>
-            <Field component="select" name="destinationMapping" onChange={handleChange} value={values.destinationMapping}>
-              <option value=""></option>
-              {destinationMappingOptions.map((mapping) => (
-                <option key={`dest-mapping-option-${mapping.id}`} value={mapping.id}>{mapping.name}</option>
-              ))}
-            </Field>
+            <div className="form-group">
+              <Label htmlFor="destinationMapping">Destination Mapping</Label>
+              <Field component="select" name="destinationMapping" onChange={handleChange} value={values.destinationMapping}>
+                <option value=""></option>
+                {destinationMappingOptions.map((mapping) => (
+                  <option key={`dest-mapping-option-${mapping.id}`} value={mapping.id}>{mapping.name}</option>
+                ))}
+              </Field>
+            </div>
 
-            <label htmlFor="frequency">Frequency</label>
-            <Field component="select" name="frequency" onChange={handleChange} value={values.frequency}>
-              <option value=""></option>
-              <option value="5 Minute">5 Minute</option>
-              <option value="Hour">Hour</option>
-              <option value="Day">Day</option>
-            </Field>
+            <div className="form-group">
+              <Label htmlFor="frequency">Frequency</Label>
+              <Field component="select" name="frequency" onChange={handleChange} value={values.frequency}>
+                <option value=""></option>
+                <option value="5 Minute">5 Minute</option>
+                <option value="Hour">Hour</option>
+                <option value="Day">Day</option>
+              </Field>
+            </div>
 
-            <label htmlFor="active">Active</label>
-            <input type="checkbox" name="active" onChange={handleChange} value={values.active} />
+            <div className="form-group">
+              <label htmlFor="active">Active</label>
+              <input type="checkbox" name="active" onChange={handleChange} value={values.active} />
+            </div>
 
             <button type="submit" disabled={isSubmitting}>
               Submit
